385aa2d9342dbad107c2fed31d0a10683b1725ae
bapnam <ndnam1411@gmail.com>
2023-01-05 15:26:58 +0700

    delete

--- React_Native/ExpoDemo_v1/view/MainView.js
+++
@@ -1,7 +0,0 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import ViewDemo from "./component/ViewDemo";
-
-export default function MainView() {
-  return <ViewDemo></ViewDemo>;
-}
